55069b7a338695d1b3eb158f7cd43ce3530e5834
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2025-10-18 10:15:39 +0100

    types and functions WIP

---
+++ pyworkshop/project.py
@@ -0,0 +1,26 @@
+
+# Integers
+X = 4
+Y = -9823
+Z = 0
+
+# Float
+A = 5.0
+B = -432.4
+C = 0.0
+
+# Strings
+MY_STRING = 'Hello'
+MY_OTHER_STRING = 'World'
+NAME = 'Jake'
+BLOOP = f"Hello, {NAME}"
+
+# exercise
+RENT = 1000
+# print(f"Hello I'm {NAME} and I pay {RENT / 30} in rent a day")
+
+# Functions
+def foo():
+    print('hello')
+
+foo()
